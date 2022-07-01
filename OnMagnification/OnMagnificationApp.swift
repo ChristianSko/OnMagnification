2337f700ae8f9218d714ceeea7d44c689dc691be
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-01 13:49:50 +0200

    Initial Commit

---
+++ OnMagnification/OnMagnificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnMagnificationApp.swift
+//  OnMagnification
+//
+//  Created by Skorobogatow, Christian on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct OnMagnificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
